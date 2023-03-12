256bdfa4cd8d9d18653cf28761cb214d20adf035
Ivanir Efi <efi.ivanir@gmail.com>
2023-03-12 13:00:37 +0200

    Add my_emacs

---
+++ my_emacs/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#262626" "#d70000" "#5f8700" "#af8700" "#0087ff" "#af005f" "#00afaf" "#626262"])
+ '(auth-source-save-behavior nil)
+ '(cursor-color nil)
+ '(foreground-color nil)
+ '(org-agenda-files
+   '("~/org/stam.org" "/home/ivanir/Prog/org-mode-tutorial/1.org"))
+ '(package-selected-packages
+   '(leuven-theme csv-mode pandoc-mode shift-number markdown-mode ox-clip htmlize org-preview-html ox-twbs org-download origami org-super-agenda yankpad yasnippet org-bullets org-cliplink aggressive-indent comment-dwim-2 linum-relative rainbow-delimiters smartparens restclient align-cljlet flycheck-clojure flycheck ibuffer-projectile projectile git-auto-commit-mode magit elisp-slime-nav cider clojure-mode eww-lnum emojify beginend selected goto-chg interaction-log discover-my-major which-key edit-list iedit highlight-symbol visual-regexp hungry-delete easy-kill browse-kill-ring expand-region ace-link avy-zap ace-window avy smex flx-ido ido-vertical-mode ivy-rich all-the-icons counsel zoom deadgrep dired-collapse dired-quick-sort dired-subtree dired-filter f esh-autosuggest counsel-osx-app async use-package))
+ '(warning-suppress-types '((comp) (comp) (comp) (comp) (comp) (comp) (comp) (comp))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
